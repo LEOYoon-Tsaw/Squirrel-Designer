3c3d78c5b9ca2b21ce7cd198f9fcea3d8e346fbe
LEOYoon-Tsaw <leoyoontsaw@icloud.com>
2020-08-31 15:23:58 -0400

    Initial Commit

---
+++ Squirrel Designer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Squirrel Designer
+//
+//  Created by LEO Yoon-Tsaw on 8/31/20.
+//  Copyright © 2020 Yuncao Liu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
